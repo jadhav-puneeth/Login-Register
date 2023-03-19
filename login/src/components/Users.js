589e097f0c511a80ec16e089a9e1df66df9fb2d2
NitinReddy01 <nitingogula@gmail.com>
2023-03-19 18:53:43 +0530

    added search

--- login/src/components/Users.js
+++ login/src/components/Users.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 export default function UserInfo(props){
+    const [search,setSearch]=useState('');
     return (
         <div className="container my-5" style={{
             display: props.userDisplay ? '' : 'none',
         }}>
+            <input className="mb-2" type="text" value={search} onChange={(e)=>{setSearch(e.target.value)}}></input>
             <table className="table table-bordered" style={{color: "white", }}>
                 <thead>
                     <tr>
@@ -11,7 +15,7 @@ export default function UserInfo(props){
                     </tr>
                 </thead>
                 <tbody>
-                    {props.users.map(user => (
+                    {props.users.filter((user)=>user.name.includes(search)).map(user => (
                         <tr>
                             <td>{user.name}</td>
                             <td>{user.password}</td>
